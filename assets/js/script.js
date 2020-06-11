59830a6b3cf44a90b5e24fad98753e6850b8f223
sitakura <scot.itakura@gmail.com>
2020-06-11 16:00:35 -0700

    added task button creator

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+tasksToDoEl;
+
+var taskItemEl = document.createElement("li");
+taskItemEl.textContent = "hello";
+
+tasksToDoEl.appendChild(taskItemEl);
+taskItemEl.className = "task-item";
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
